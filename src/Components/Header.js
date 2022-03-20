3f9b564dfae8b6f669e72c7bc81ff5247a3203ca
harsh patel <harsh.1722.patel@gmail.com>
2022-03-20 13:24:17 +0530

    123

---
+++ src/Components/Header.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import Styles from './Header.module.css'
+import bg from "../Images/trade_bg.jpg"
+
+const Header = () => {
+  return (
+    <>
+
+    <div className={Styles.header}>
+        <div className={Styles.imgContainer}>
+            <img className={Styles.img} src={bg}/>
+        </div>
+
+        <h1 className={Styles.headerText}>
+        Trade bitcoin and other cryptocurrencies and earn real profits in the following 24 hours
+        </h1>
+    </div>
+    
+    </>
+  )
+}
+
+export default Header
